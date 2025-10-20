c5f759a30a1a22d60d0a1061ff22e7355e51b36f
Diego Benzi <diegobenzi@gmail.com>
2025-10-19 21:21:59 -0300

    change color numbers

--- desafio075.py
+++ desafio075.py
@@ -1,4 +1,6 @@
+print("=*="*10)
 print("Verificação de Dados")
+print("=*="*10)
 num1=int(input("Digite o primeiro número:"))
 num2=int(input("Digite o segundo número:"))
 num3=int(input("Digite o terceiro número:"))
@@ -6,12 +8,12 @@ num4=int(input("Digite o quarto e último número:"))
 dados=(num1,num2,num3,num4)
 cont=0
 print(f"Você digitou os valores: {dados}")
-print(f"O valor 9 apareceu {dados.count(9)} vezes!")
+print(f"O valor 9 apareceu \33[32m{dados.count(9)} \33[mvezes!")
 if 3 in dados:
-    print(f"O número 3 apareceu na {dados.index(3)+1}º posição.")
+    print(f"O número 3 apareceu na \33[35m{dados.index(3)+1}º\33[m posição.")
 else:
    print("O valor 3 não foi encontrado nessa sequência!")
-print(f"Os valores pares digitados foram:",end="")
+print("Os valores pares digitados foram:",end="")
 for num in dados:
    if num %2 == 0:    
     print(num,end=",")
